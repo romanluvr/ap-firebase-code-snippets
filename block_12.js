f359a710de8c8000d5aa8566faa8816d087c7322
Roman Lutsiv <roman.lutsiv@spartez-software.com>
2021-09-21 15:00:10 +0200

    removed redundant commas

--- block_12.js
+++ block_12.js
@@ -1,7 +1,8 @@
+:i
 async mounted() {
     try {
         this.interactiveSessionUnsubscribe = await this.subscribeToInteractiveSessionChanges();
     } catch (error) {
         throw new NestedError('Failed to subscribe to Firebase', error);
     }
-},
+}
